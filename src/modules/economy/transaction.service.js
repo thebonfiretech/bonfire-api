4b7944caf7af17e62d8c735f9b6ade907ba1548e
dutra-pc <joao.vitornl@gmail.com>
2024-06-20 14:52:19 -0300

    :bug: fix: Replace variables

--- src/modules/economy/transaction.service.js
+++ src/modules/economy/transaction.service.js
@@ -6,30 +6,31 @@ export default class Service {
     async createTransaction({ from, to, coins }){
         try {
             if (!from || !to || !coins) return { error: "invalid_data" }
-            const fromUser = await userModel.findById(from).select("-password");
+            var fromUser = await userModel.findById(from).select("-password");
             if (!fromUser) return { error: "user_not_found" };
             if (fromUser.coins < coins) return { error: "insufficient_coins" };
-            const toUser = await userModel.findById(to).select("-password");
+            var toUser = await userModel.findById(to).select("-password");
             if (!toUser) return { error: "user_not_found" };
-            fromUser -= coins;
-            await fromUser.save()
-            toUser += coins;
-            await toUser.save()
+            fromUser.coins -= Number(coins);
+            await fromUser.save(); 
+            toUser.coins += Number(coins);
+            await toUser.save(); 
             const fromExtract = await extractModel.create({
                 author: from,
                 value: -coins,
                 description: `Transferência para ${toUser.name}`,
                 type: 'transaction'
-            })
+            }); 
             const toExtract = await extractModel.create({
-                author: from,
-                value: -coins,
-                description: `Transferência para ${toUser.name}`,
+                author: to,
+                value: coins,
+                description: `Recebido de  ${fromUser.name}`,
                 type: 'transaction'
-            })
+            });
             return { from: fromUser, to: toUser, fromExtract, toExtract }
 
         } catch (err) {
+            console.log(err)
             return { error: "internal_error" } ;
         }
     }
--- src/routes/resources/transaction.router.js
+++ src/routes/resources/transaction.router.js
@@ -2,5 +2,5 @@ import createRouter from "../../middlewares/createRouter.js";
 import service from "../../modules/economy/transaction.service.js";
 
 export default createRouter(service, [
-    ["post", "/create", "createFavorites", true],
+    ["post", "/create", "createTransaction", true],
 ]);
