b8ed8d593b4f4b794f166ef069b35e874fead288
dutra-pc <joao.vitornl@gmail.com>
2024-12-30 16:59:08 -0300

    feat: Create control access midleware

--- src/assets/config/errors.ts
+++ src/assets/config/errors.ts
@@ -1,17 +1,59 @@
 interface ResponseError {
-    statusCode: 100 | 101 | 102 | 200 | 201 | 202 | 204 | 301 | 302 | 304 | 400 | 401 | 403 | 404 | 500 | 501 | 502 | 503;
+    statusCode: 100 | 101 | 102 | 200 | 201 | 202 | 204 | 301 | 302 | 304 | 400 | 401 | 403 | 404 | 429 | 500 | 501 | 502 | 503;
     message: string;
 };
 
-export type ResponseErrorsParams = "internal_error" | "not_user_email";
+export type ResponseErrorsParams = 
+| "admin_access_denied"
+| "no_credentials_send" 
+| "invalid_credentials" 
+| "token_is_not_valid"
+| "internal_error" 
+| "invalid_params" 
+| "user_not_found" 
+| "access_denied"
+| "no_data_send" 
+| "no_token";
 
 export const ResponseErrors: Record<ResponseErrorsParams, ResponseError> = {
     internal_error: {
         message: "Server Error",
         statusCode: 500
     },
-    not_user_email: {
-        message: "Not user email",
+    no_credentials_send: {
+        message: "No credentials send",
         statusCode: 401
+    },
+    no_data_send: {
+        message: "No data send",
+        statusCode: 400
+    },
+    invalid_credentials: {
+        message: "Invalid Credentials",
+        statusCode: 401
+    },
+    no_token: {
+        message: "No token, authorization denied",
+        statusCode: 400
+    },
+    token_is_not_valid: {
+        message: "Token is not valid",
+        statusCode: 401
+    },
+    user_not_found: {
+        message: "User not found",
+        statusCode: 404
+    },
+    access_denied: {
+        message: "Access Denied",
+        statusCode: 401
+    },
+    admin_access_denied: {
+        message: "No admin, Access Denied",
+        statusCode: 401
+    },
+    invalid_params: {
+        message: "Invalid params send",
+        statusCode: 400
     },
 };
--- src/resources/users/users.resource.ts
+++ src/resources/users/users.resource.ts
@@ -3,7 +3,6 @@ import { ManageRequestBody } from "@middlewares/manageRequest";
 const usersResource = {
     getUser: async ({ data, manageError }: ManageRequestBody) => {
         try {
-            if (!data.email) return manageError({ code: "not_user_email"});
             return data;
         } catch (error) {
             manageError({ code: "internal_error", error });
