7d01c32a7f9924ff3a7c3a1b534c51af8486dfd9
dutra-pc <joao.vitornl@gmail.com>
2024-12-30 22:40:25 -0300

    feat: Create user model

---
+++ src/database/model/user.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    id: {
+        unique: true,
+        type: String
+    },
+    name: String,
+    role: {
+        enum: ["normal", "admin"],
+        default: "normal",
+        type: String,
+    },
+    status: {
+        enum: ["loggedIn", "registered", "pending", "blocked"],
+        default: "registered",
+        type: String,
+    },
+    createAt: {
+        default: Date.now(),
+        type: Date,
+    },
+    lastUpdate: {
+        type: Date,
+    },
+    spaces: [{
+        entryAt: Date,
+        role: {
+            type: String,
+            enum: ["normal", "editor", "admin"],
+            default: "normal"
+        },
+        name: String,
+        id: String 
+    }],
+    description: {
+        type: String
+    },
+    images: {
+        profile: String
+    },
+    badges: [],
+    coins: {
+        type: Number,
+        default: 0
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    }
+});
+
+const usersModel = mongoose.model("user", userSchema);
+
+export default usersModel;
