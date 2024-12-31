41893e4e46932e09e93ea9ecaddef5edf6d64cce
dutra-pc <joao.vitornl@gmail.com>
2024-12-30 23:48:51 -0300

    feat: Create users router

---
+++ src/routes/resources/users.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import usersResource from "@resources/users/users.resource";
+import manageRequest from "@middlewares/manageRequest";
+
+const usersRouter = Router();
+
+usersRouter.post("/", manageRequest(usersResource.getUser))
+
+export default usersRouter;
