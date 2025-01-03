8f5051823db22f91b02db96e0aa10ef98e8e5cff
dutra-pc <joao.vitornl@gmail.com>
2025-01-02 22:46:23 -0300

    feat: Add mongoose object id validation

--- src/database/functions/space.ts
+++ src/database/functions/space.ts
@@ -1,12 +1,16 @@
+import { isValidObjectId } from "mongoose";
+
 import spaceModel, { SpaceModelType } from "@database/model/space";
 
 export const hasSpaceAlreadyExists = async (space: Partial<SpaceModelType>, manageError: Function) => {
+    if (space._id && !isValidObjectId(space._id)) return manageError({ code: "space_not_found" }); 
     const hasSpace = await spaceModel.findOne(space);
     if (hasSpace) return manageError({ code: "space_already_exists" });
     return hasSpace;
 };
 
 export const hasNoSpaceAlreadyExists = async (space: Partial<SpaceModelType>, manageError: Function) => {
+    if (space._id && !isValidObjectId(space._id)) return manageError({ code: "space_not_found" }); 
     const hasSpace = await spaceModel.findOne(space);
     if (!hasSpace) return manageError({ code: "space_not_found" });
     return hasSpace;
--- src/database/functions/user.ts
+++ src/database/functions/user.ts
@@ -1,12 +1,16 @@
+import { isValidObjectId } from "mongoose";
+
 import userModel, { UserModelType } from "@database/model/user";
 
 export const hasUserAlreadyExists = async (user: Partial<UserModelType>, manageError: Function) => {
+    if (user._id && !isValidObjectId(user._id)) return manageError({ code: "user_not_found" }); 
     const hasUser = await userModel.findOne(user).select("-passoword");
     if (hasUser) return manageError({ code: "user_already_exists" });
     return hasUser;
 };
 
 export const hasNoUserAlreadyExists = async (user: Partial<UserModelType>, manageError: Function) => {
+    if (user._id && !isValidObjectId(user._id)) return manageError({ code: "user_not_found" }); 
     const hasUser = await userModel.findOne(user).select("-passoword");
     if (!hasUser) return manageError({ code: "user_not_found" });
     return hasUser;
