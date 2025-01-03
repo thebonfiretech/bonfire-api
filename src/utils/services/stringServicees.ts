2fa06789ed798ace26d439a903e74e617b654a41
dutra-pc <joao.vitornl@gmail.com>
2025-01-03 19:08:22 -0300

    feat: Create removeSpacesAndLowerCase function

--- src/utils/services/stringServicees.ts
+++ src/utils/services/stringServicees.ts
@@ -5,11 +5,13 @@ const stringService = {
             .replace(/\s+/g, ' ')
             .toLowerCase();
     },
-
     capitalizeFirstLetters: (str: string): string => {
         return str
             .toLowerCase()
             .replace(/(^|\.\s*)([a-z])/g, (match, p1, p2) => p1 + p2.toUpperCase());
+    },
+    removeSpacesAndLowerCase: (str: string): string => {
+        return str.replace(/\s+/g, '').toLowerCase();
     }
 };
 
