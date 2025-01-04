4d19e96748d24ba9e6d467146c3778d9069d6ca8
dutra-pc <joao.vitornl@gmail.com>
2025-01-03 22:02:25 -0300

    feat: Create key model type

---
+++ src/utils/types/models/key.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+
+export interface KeyModelType {
+    _id: string;
+    name: string;
+    createAt?: Date;
+    lastUpdate?: Date;
+    description?: string;
+    keyType: "pix" | "favorite";
+    userID: Types.ObjectId;
+};
