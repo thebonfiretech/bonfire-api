eaf181182373c454ccce23caa998467f66fd42cd
dutra-pc <joao.vitornl@gmail.com>
2025-01-04 15:14:46 -0300

    feat: Finish createClient and checkStorageConnection functions

--- src/database/storage.ts
+++ src/database/storage.ts
@@ -1,31 +1,49 @@
+import { S3Client, HeadBucketCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import defaultConfig from "@assets/config/default";
 import logger from "@utils/functions/logger";
 
 const storage = {
-    checkStorageConnect: async () => {
+    createClient: async (check?: boolean) => {
         try {            
-            const STORAGE_URL = defaultConfig.mode == "developing" ? process.env.STORAGE_URL_DEV : process.env.STORAGE_URL_PRD;
-            if (!STORAGE_URL) {
-                logger.error("[checkStorageConnect] No storage URI");
+            const STORAGE_URL = defaultConfig.mode === "developing" ? process.env.STORAGE_URL_DEV : process.env.STORAGE_URL_PRD;
+            const STORAGE_BUCKET = defaultConfig.mode === "developing" ? process.env.STORAGE_BUCKET_NAME_DEV : process.env.STORAGE_BUCKET_NAME_PRD;
+
+            if (!STORAGE_URL || !STORAGE_BUCKET) {
+                logger.error("[createClient] Storage URI or Bucket Name is missing.");
                 process.exit(1);
-            };
-            const STORAGE_BUCKET = defaultConfig.mode == "developing" ? process.env.STORAGE_BUCKET_NAME_DEV : process.env.STORAGE_BUCKET_NAME_PRD;
-            if (!STORAGE_BUCKET) {
-                logger.error("[checkStorageConnect] No storage bucket name");
-                process.exit(1);
-            };
+            }
 
+            const s3Client = new S3Client({
+                endpoint: STORAGE_URL,
+                region: "br-se1",
+                credentials: {
+                    accessKeyId: process.env.STORAGE_ID || "",
+                    secretAccessKey: process.env.STORAGE_SECRET || "",
+                },
+            });
 
-            logger.info("Storage connected: " + STORAGE_BUCKET);
-            
-            return { STORAGE_BUCKET };
+            if (check) {
+                logger.info(`Storage connected: ${STORAGE_BUCKET}`);
+            }
 
+            return { s3Client, bucket: STORAGE_BUCKET };
         } catch (error) {
-            logger.error("[checkStorageConnect] Database connect error");
-            console.log(error);
+            logger.error("[createClient] Bucket connection error.");
+            console.error(error);
             process.exit(1);
         }
-    }
+    },
+    checkStorageConnect: async () => {
+        try {
+            const { s3Client, bucket } = await storage.createClient(true);
+            const command = new HeadBucketCommand({ Bucket: bucket });
+            await s3Client.send(command);
+        } catch (error) {
+            logger.error("[checkStorageConnect] Internal error.");
+            console.error(error);
+            process.exit(1);
+        }
+    },
 };
 
 export default storage;
