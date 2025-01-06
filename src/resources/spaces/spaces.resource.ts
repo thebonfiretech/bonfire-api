6aa6a2e92a6206fe94707a6c3d8e7888a4f1abdf
dutra-pc <joao.vitornl@gmail.com>
2025-01-05 23:53:40 -0300

    feat: Finish getSpace function

--- src/resources/spaces/spaces.resource.ts
+++ src/resources/spaces/spaces.resource.ts
@@ -1,9 +1,13 @@
+import { hasSpace } from "@database/functions/space";
 import { ManageRequestBody } from "@middlewares/manageRequest";
 
 const spacesResource = {
-    getSpace: async ({ manageError }: ManageRequestBody) => {
+    getSpace: async ({ manageError, params }: ManageRequestBody) => {
         try {
+            const { spaceID } =  params;
+            if (!spaceID) return manageError({ code: "invalid_params" });
 
+            return await hasSpace({ _id: spaceID }, manageError);
         } catch (error) {
             manageError({ code: "internal_error", error });
         }
