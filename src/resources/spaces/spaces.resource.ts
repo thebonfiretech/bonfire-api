9e8d2aa8fb66905618bbba86cc45352620e7c3b0
dutra-pc <joao.vitornl@gmail.com>
2025-01-05 23:59:30 -0300

    feat: Finish getSpaceUsers function

--- src/resources/spaces/spaces.resource.ts
+++ src/resources/spaces/spaces.resource.ts
@@ -1,4 +1,6 @@
 import { hasSpace } from "@database/functions/space";
+import spaceModel from "@database/model/space";
+import userModel from "@database/model/user";
 import { ManageRequestBody } from "@middlewares/manageRequest";
 
 const spacesResource = {
@@ -8,6 +10,19 @@ const spacesResource = {
             if (!spaceID) return manageError({ code: "invalid_params" });
 
             return await hasSpace({ _id: spaceID }, manageError);
+        } catch (error) {
+            manageError({ code: "internal_error", error });
+        }
+    },
+    getSpaceUsers: async ({ manageError, params }: ManageRequestBody) => {
+        try {
+            const { spaceID } =  params;
+            if (!spaceID) return manageError({ code: "invalid_params" });
+
+            const space = await hasSpace({ _id: spaceID }, manageError);
+            if (!space) return;
+
+            return await userModel.find({ "spaces.id": spaceID }).select("-password");
         } catch (error) {
             manageError({ code: "internal_error", error });
         }
--- src/routes/resources/spaces.router.ts
+++ src/routes/resources/spaces.router.ts
@@ -5,6 +5,7 @@ import manageRequest from "@middlewares/manageRequest";
 
 const spacesRouter = Router();
 
+spacesRouter.get("/:spaceID/users", manageRequest(spacesResource.getSpaceUsers));
 spacesRouter.get("/:spaceID", manageRequest(spacesResource.getSpace));
 
 export default spacesRouter;
