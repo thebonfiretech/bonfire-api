f22a95c3097544860372b9489cfc354f7af344c8
dutra-pc <joao.vitornl@gmail.com>
2025-01-12 13:31:44 -0300

    feat: Create ticket model

---
+++ src/database/model/ticket.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    createAt: {
+        default: Date.now(),
+        type: Date,
+    },
+    lastUpdate: {
+        type: Date,
+    },
+    description: {
+        type: String
+    },
+    slotType: {
+        type: String,
+        enum: ["suggestion", "report", "feedback", "other"],
+    },
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "user"
+    },
+    messages: {
+        type: [{
+            content: String,
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user"
+            },
+            date: {
+                default: Date.now(),
+                type: Date
+            }
+        }]
+    },
+    attachments: {
+        type: [String],
+        default: []
+    }
+});
+
+const ticketModel = mongoose.model("ticket", ticketSchema);
+
+export default ticketModel;
