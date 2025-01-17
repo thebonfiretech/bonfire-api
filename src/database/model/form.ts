69138a7a3b975d0e828620af2ae96ca97ffc4c47
odutradev <joao.vitornl@gmail.com>
2025-01-17 12:37:23 -0200

    feat: Create form model

---
+++ src/database/model/form.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const answerSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ["text", "multiple-choice", "single-choice"],
+        required: true,
+    },
+    answer: {
+        type: [
+            {
+                text: {
+                    type: String,
+                    required: true,
+                },
+                id: String
+            },
+        ],
+        default: [],
+    }
+});
+
+const formSchema = new mongoose.Schema({
+    formControlID: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "form-control",
+    },
+    submittedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        },
+        avatar: String,
+        name: String,
+        email: String
+    },
+    answers: {
+        type: [answerSchema],
+        default: [],
+    },
+});
+
+const formControlModel = mongoose.model("form", formSchema);
+
+export default formControlModel;
